fa59217eca18ccc5fec2026a1a372a2ec96318a5
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-07-01 16:06:58 +0300

    Create api to list pizzas

---
+++ BlazorApp2-Pizza/Data/PizzaService.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp2_Pizza.Data
+{
+    public class PizzaService
+    {
+        public Task<Pizza[]> GetPizzasAsync()
+        {
+            var pizzas = new Pizza[]
+            {
+                new Pizza(){ Name = "Margharitta",Description = "Vkusna", Price = 9.50m,Vegan = true,Vegetarian = false},              
+                new Pizza(){ Name = "Margharitta",Description = "Vkusna", Price = 9.50m,Vegan = true ,Vegetarian = false},
+                new Pizza(){ Name = "Margharitta",Description = "Vkusna", Price = 9.50m,Vegan = true,Vegetarian = false},
+                new Pizza(){ Name = "Margharitta",Description = "Vkusna", Price = 9.50m,Vegan = true, Vegetarian = false}
+            };
+
+            return Task.FromResult(pizzas);
+        }
+    }
+}
